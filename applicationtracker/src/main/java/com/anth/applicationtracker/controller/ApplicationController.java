1784ed298f0f1c0f6dd1e7c5f110f32d4f1588ce
Anthony Chapman <anthonychapman535@gmail.com>
2025-06-14 21:25:39 -0500

    added basic authentication and pagination for applications

--- applicationtracker/src/main/java/com/anth/applicationtracker/controller/ApplicationController.java
+++ applicationtracker/src/main/java/com/anth/applicationtracker/controller/ApplicationController.java
@@ -4,10 +4,19 @@ import com.anth.applicationtracker.exception.IdMismatchException;
 import com.anth.applicationtracker.exception.NotFoundException;
 import com.anth.applicationtracker.model.Application;
 import com.anth.applicationtracker.repo.ApplicationRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/applications")
@@ -18,20 +27,36 @@ public class ApplicationController {
         this.applicationRepository = applicationRepository;
     }
 
-    @GetMapping
-    public Iterable<Application> findAll() {
-        return applicationRepository.findAll();
+    @GetMapping()
+    private ResponseEntity<List<Application>> findAll(Pageable pageable) {
+        Page<Application> page = applicationRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "submitDate"))
+                )
+        );
+        return ResponseEntity.ok(page.getContent());
     }
 
     @GetMapping("/{id}")
-    public Application findOne(@PathVariable Long id) {
-        return applicationRepository.findById(id).orElseThrow(NotFoundException::new);
+    private ResponseEntity<Application> findById(@PathVariable Long id) {
+        Optional<Application> applicationOptional = applicationRepository.findById(id);
+        if (applicationOptional.isPresent()) {
+            return ResponseEntity.ok(applicationOptional.get());
+        } else {
+            throw new NotFoundException();
+        }
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Application create(@RequestBody Application application) {
-        return applicationRepository.save(application);
+    private ResponseEntity<Void> createApplication(@RequestBody Application newApplicationRequest, UriComponentsBuilder ucb) {
+        Application savedApplication = applicationRepository.save(newApplicationRequest);
+        URI locationOfNewApplication = ucb
+                .path("applications/{id}")
+                .buildAndExpand(savedApplication.getId())
+                .toUri();
+        return ResponseEntity.created(locationOfNewApplication).build();
     }
 
     @DeleteMapping("/{id}")
--- applicationtracker/src/main/java/com/anth/applicationtracker/repo/ApplicationRepository.java
+++ applicationtracker/src/main/java/com/anth/applicationtracker/repo/ApplicationRepository.java
@@ -2,8 +2,9 @@ package com.anth.applicationtracker.repo;
 
 import com.anth.applicationtracker.model.Application;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ApplicationRepository extends CrudRepository<Application, Long> {
+public interface ApplicationRepository extends CrudRepository<Application, Long>, PagingAndSortingRepository<Application, Long> {
 }
